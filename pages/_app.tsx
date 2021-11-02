6c9254f6c0d747e3111db55403d5748b35458282
leo <algo2000@naver.com>
2021-11-02 23:51:57 +0900

    up _app ThemeRoot  추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,23 @@ import type { AppProps } from "next/app";
 
 import { RecoilRoot } from "recoil";
 
+import ThemeRoot from "../theme/ThemeRoot";
+
 import Sidebar from "../components/sidebar";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <RecoilRoot>
-      <div style={{ display: "flex", maxWidth: 1100 }}>
-        <div style={{ flexBasis: "30%", margin: 25 }}>
-          <Sidebar />
+      <ThemeRoot>
+        <div style={{ display: "flex", maxWidth: 1100 }}>
+          <div style={{ flexBasis: "30%", margin: 25 }}>
+            <Sidebar />
+          </div>
+          <div style={{ flexBasis: "70%", margin: 25 }}>
+            <Component {...pageProps} />
+          </div>
         </div>
-        <div style={{ flexBasis: "70%", margin: 25 }}>
-          <Component {...pageProps} />
-        </div>
-      </div>
+      </ThemeRoot>
     </RecoilRoot>
   );
 }
